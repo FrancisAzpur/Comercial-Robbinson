58c3ca4a1ac65c5cadc8227a5ec6f6346d1644df
FrancisAzpur <u21229536@utp.edu.pe>
2026-02-11 16:09:31 -0500

    Fix

--- src/main/java/com/Robbinson/ComRobinson/controladores/GestionController.java
+++ src/main/java/com/Robbinson/ComRobinson/controladores/GestionController.java
@@ -375,7 +375,7 @@ public class GestionController {
      */
     @GetMapping("/pedidos")
     public String listarPedidos(Model modelo) {
-        List<Pedido> pedidos = pedidoService.obtenerTodosLosPedidos("");
+        List<Pedido> pedidos = pedidoService.obtenerPedidosPorEstado("");
         int[] conteos = pedidoService.contarPedidosPorEstado();
 
         modelo.addAttribute("pedidos", pedidos);
